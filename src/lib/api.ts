78ae7400f6be3b6a96500759de36722a71281964
divyab979 <divyab979@gmail.com>
2026-06-10 14:14:19 +0530

    chore: update API baseURL to the production domain

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,7 +6,7 @@ const isLocalhost =
    window.location.hostname === "127.0.0.1" || 
    window.location.hostname.startsWith("192.168."));
 export const api = axios.create({
-  baseURL: "http://15.206.136.241:4000",
+  baseURL: "https://api.gameup11.com",
 });
 
 api.interceptors.request.use((config) => {
